59f298a86380fa0cad6d1e38e5a7c0fcf26d483b
klommothy <xxdarkxidexx@gmail.com>
2024-07-01 22:31:29 +0200

    simplify unnecessary logic

--- src/objects/CollidableObject.ts
+++ src/objects/CollidableObject.ts
@@ -4,19 +4,13 @@ export default class CollidableObject extends Phaser.Physics.Arcade.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'CollidableObject');
     scene.add.existing(this);
-
     scene.physics.add.existing(this);
 
-    if (this.body instanceof Phaser.Physics.Arcade.Body) {
-      const body = this.body;
-
-      body.setGravity(0, 0); // Disable gravity for this object
-      body.setBounce(0.8, 0.8); // Enable bouncing
-      body.setDrag(50,50)
-      body.setImmovable(false); 
-    }
-
-    this.setInteractive();
+    this.setGravity(0, 0);
+    this.setBounce(0.8, 0.8); 
+    this.setDrag(50, 50);
+    this.setImmovable(false);
     this.setCollideWorldBounds(true);
+    this.setInteractive();
   }
 }
