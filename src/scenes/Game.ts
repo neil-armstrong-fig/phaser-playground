f1d119a4ee4fa32cb46d02e3f6aba6dc06105790
klommothy <xxdarkxidexx@gmail.com>
2024-07-01 22:12:13 +0200

    update scenes to allow for collidableobject to exist

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,6 +1,7 @@
 import {Scene} from "phaser";
 import FpsText from '../objects/fpsText';
 import LaunchableObject from "../objects/LaunchableObject";
+import CollidableObject from "../objects/CollidableObject";
 
 export class Game extends Scene {
   camera: Phaser.Cameras.Scene2D.Camera;
@@ -8,6 +9,7 @@ export class Game extends Scene {
   msg_text: Phaser.GameObjects.Text;
   fpsText: FpsText;
   launchableObject: LaunchableObject;
+  collidableObject: CollidableObject;
 
   constructor() {
     super("Game");
@@ -22,6 +24,8 @@ export class Game extends Scene {
     this.background.setAlpha(0.5);
 
     this.launchableObject = new LaunchableObject(this, this.cameras.main.width / 4, this.cameras.main.height / 2);
+  
+    this.collidableObject = new CollidableObject(this, 500, 300);
   }
 
   update() {
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -26,8 +26,9 @@ export class Preloader extends Scene {
     //  Load the assets for the game - Replace with your own assets
     this.load.setPath("assets");
     this.load.image("logo", "logo.png");
-    
+
     this.load.image("LaunchableObject", "LaunchableObject.png");
+    this.load.image("CollidableObject", "CollidableObject.png")
   }
 
   create() {
