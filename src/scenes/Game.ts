ff3bcaf7810a37b83f64ad2d13d88421fb0ed02f
klommothy <xxdarkxidexx@gmail.com>
2024-07-02 21:46:58 +0200

    went back to eating my own ass and dug even further

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,52 +1,73 @@
-import {Scene} from "phaser";
-import FpsText from '../objects/fpsText';
+import { Scene } from "phaser";
+import FpsText from "../objects/fpsText";
 import LaunchableObject from "../objects/LaunchableObject";
 import CollidableObject from "../objects/CollidableObject";
 
 export class Game extends Scene {
-  camera: Phaser.Cameras.Scene2D.Camera;
-  background: Phaser.GameObjects.Image;
-  msg_text: Phaser.GameObjects.Text;
-  fpsText: FpsText;
-  launchableObject: LaunchableObject;
-  collidableObject: CollidableObject;
-  collidableObjects: Phaser.Physics.Arcade.Group;
+	camera: Phaser.Cameras.Scene2D.Camera;
+	background: Phaser.GameObjects.Image;
+	msg_text: Phaser.GameObjects.Text;
+	fpsText: FpsText;
+	launchableObject: LaunchableObject;
+	collidableObject: CollidableObject;
+	collidableObjects: Phaser.Physics.Arcade.Group;
 
-  constructor() {
-    super("Game");
-  }
+	constructor() {
+		super("Game");
+	}
 
-  create() {
-    this.camera = this.cameras.main;
-    this.camera.setBackgroundColor(0x00ff00);
-    this.background = this.add.image(512, 384, "background");
-    this.background.setAlpha(0.5);
+	create() {
+		this.camera = this.cameras.main;
+		this.camera.setBackgroundColor(0x00ff00);
+		this.background = this.add.image(512, 384, "background");
+		this.background.setAlpha(0.5);
 
-    this.physics.world.setBounds(0, 0, this.scale.width, this.scale.height);
-    this.fpsText = new FpsText(this)
+		this.physics.world.setBounds(0, 0, this.scale.width, this.scale.height);
+		this.fpsText = new FpsText(this);
 
-    this.launchableObject = new LaunchableObject(this, this.cameras.main.width / 4, this.cameras.main.height / 2);
-    this.collidableObject = new CollidableObject(this, 500, 300);
-    this.collidableObject = new CollidableObject(this, 700, 300);
-    this.collidableObject = new CollidableObject(this, 900, 300);
+		this.launchableObject = new LaunchableObject(
+			this,
+			this.cameras.main.width / 4,
+			this.cameras.main.height / 2,
+		);
 
+		this.collidableObjects = this.physics.add.group();
 
-  this.physics.add.collider(
-    this.launchableObject,
-    this.collidableObject,
-  );
+		// Add multiple CollidableObjects to the group
+		const positions = [
+			{ x: 500, y: 300 },
+			{ x: 700, y: 400 },
+			{ x: 900, y: 500 },
+		];
 
- // I don't know why this looks weird, but it does
-  this.physics.add.collider(
-    this.collidableObject, 
-    this.collidableObject
-  );
+		for (const pos of positions) {
+			const collidableObject = new CollidableObject(this, pos.x, pos.y);
+			this.collidableObjects.add(collidableObject);
+		}
 
-  // Debugging to figure out world bounds
-  this.physics.world.createDebugGraphic();
-  }
+		// Configure properties for each collidable object in the group
+		this.configureCollidableObjects();
 
-  update() {
-    this.fpsText.update();
-  }
+		// Add colliders
+		this.physics.add.collider(this.launchableObject, this.collidableObjects);
+		this.physics.add.collider(this.collidableObjects, this.collidableObjects);
+
+		// Debugging to figure out world bounds
+		this.physics.world.createDebugGraphic();
+	}
+
+	configureCollidableObjects() {
+		for (const collidableObject of this.collidableObjects.getChildren() as CollidableObject[]) {
+			collidableObject.setGravity(0, 0);
+			collidableObject.setBounce(0.8, 0.8);
+			collidableObject.setDrag(50, 50);
+			collidableObject.setImmovable(false);
+			collidableObject.setCollideWorldBounds(true);
+			collidableObject.setInteractive();
+		}
+	}
+
+	update() {
+		this.fpsText.update();
+	}
 }
