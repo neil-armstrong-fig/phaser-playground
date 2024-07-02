1e82651203e626293fa9421b41d562edd41cd20a
klommothy <xxdarkxidexx@gmail.com>
2024-07-02 21:09:03 +0200

    exited out my own ass and simplified instantiation of collidableobject to make it actually inherit the properties of a collidableobject

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -9,8 +9,8 @@ export class Game extends Scene {
   msg_text: Phaser.GameObjects.Text;
   fpsText: FpsText;
   launchableObject: LaunchableObject;
+  collidableObject: CollidableObject;
   collidableObjects: Phaser.Physics.Arcade.Group;
-
 
   constructor() {
     super("Game");
@@ -19,33 +19,32 @@ export class Game extends Scene {
   create() {
     this.camera = this.cameras.main;
     this.camera.setBackgroundColor(0x00ff00);
-
     this.background = this.add.image(512, 384, "background");
     this.background.setAlpha(0.5);
 
+    this.physics.world.setBounds(0, 0, this.scale.width, this.scale.height);
     this.fpsText = new FpsText(this)
-    this.launchableObject = new LaunchableObject(this, this.cameras.main.width / 4, this.cameras.main.height / 2);
-  
-    this.collidableObjects = this.physics.add.group({
-      classType: CollidableObject,
-      runChildUpdate: true,
-    });
 
-    // Add collidable objects to the group
-    this.collidableObjects.add(new CollidableObject(this, 500, 300));
-    this.collidableObjects.add(new CollidableObject(this, 700, 400));
-    this.collidableObjects.add(new CollidableObject(this, 900, 500));
+    this.launchableObject = new LaunchableObject(this, this.cameras.main.width / 4, this.cameras.main.height / 2);
+    this.collidableObject = new CollidableObject(this, 500, 300);
+    this.collidableObject = new CollidableObject(this, 700, 300);
+    this.collidableObject = new CollidableObject(this, 900, 300);
+
 
   this.physics.add.collider(
     this.launchableObject,
-    this.collidableObjects,
+    this.collidableObject,
   );
 
+ // I don't know why this looks weird, but it does
   this.physics.add.collider(
-    this.collidableObjects,
-    this.collidableObjects
-  ); // I don't know why this looks weird, but it does
-}
+    this.collidableObject, 
+    this.collidableObject
+  );
+
+  // Debugging to figure out world bounds
+  this.physics.world.createDebugGraphic();
+  }
 
   update() {
     this.fpsText.update();
